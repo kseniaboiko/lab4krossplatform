9567dc8be9025dbecd575788dd229373b2403988
Kseniia <kseniia@adminpdfs-MacBook-Pro-8.local>
2020-03-05 14:09:20 +0200

    first commit

---
+++ src/main/java/com/example/kross_4/Kross4Application.java
@@ -0,0 +1,13 @@
+package com.example.kross_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kross4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kross4Application.class, args);
+	}
+
+}
